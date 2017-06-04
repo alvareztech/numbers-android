cdb6868f79fe5042c1e02027324c7207e573a905
Daniel Alvarez <a3dany@gmail.com>
2017-06-04 15:37:24 -0400

    Tests

--- app/src/androidTest/java/tech/alvarez/numbers/MainActivityScreenTest.java
+++ app/src/androidTest/java/tech/alvarez/numbers/MainActivityScreenTest.java
@@ -4,29 +4,28 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import tech.alvarez.numbers.activities.MainActivity;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
 
 @RunWith(AndroidJUnit4.class)
-public class ApplicationTest {
+public class MainActivityScreenTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
-    @Test
-    public void clickAddButton_showActivity() {
-        onView(withId(R.id.fab)).perform(click());
+    public void ok() {
+        onData(anything()).inAdapterView(withId(R.id.rankingRecyclerView)).atPosition(0).perform(click());
 
-        pressBack();
-
-        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.nameTextView)).check(matches(withText("ALVAREZ.tech")));
     }
 
 }
---
+++ app/src/androidTest/java/tech/alvarez/numbers/activities/MainActivityTest.java
@@ -0,0 +1,84 @@
+package tech.alvarez.numbers.activities;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import tech.alvarez.numbers.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+
+        onView(withId(R.id.fab)).perform(click());
+
+        onView(withId(R.id.queryEditText)).perform(replaceText("alvarez tech"), closeSoftKeyboard());
+
+        onView(withId(R.id.queryEditText)).perform(pressImeActionButton());
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.titleTextView), withText("ALVAREZ.tech"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        1),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("ALVAREZ.tech")));
+
+    }
+
+    private static Matcher<View> childAtPosition(final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
