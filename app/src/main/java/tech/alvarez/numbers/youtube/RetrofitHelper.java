8101a75ca88161fe2289bcdd75f7c4456e463713
Daniel Alvarez <daniel@alvarez.tech>
2020-04-01 18:02:16 -0400

    Kotlin migration

--- app/src/main/java/tech/alvarez/numbers/youtube/RetrofitHelper.java
+++
@@ -1,9 +0,0 @@
-package tech.alvarez.numbers.youtube;
-
-public class RetrofitHelper {
-
-    public static YouTubeDataApiService getYouTubeService() {
-        return RetrofitClient.getClient().create(YouTubeDataApiService.class);
-    }
-
-}
